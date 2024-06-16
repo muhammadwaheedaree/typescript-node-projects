79917c6e89a6e12313e7fd31d2949a96f2b97c2d
Muhammad waheed <162149796+muhammadwaheedaree@users.noreply.github.com>
2024-06-16 21:15:29 +0500

    Add files via upload

---
+++ project01_number_guessing-game/README.md
@@ -0,0 +1,5 @@
+# cli-number-guessing
+"CLI Number Guessing Game in TypeScript. 
+Run the game, guess the number, and see 
+if you're correct! Built with TypeScript 
+         for fun and learning."
---
+++ project01_number_guessing-game/index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env  node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 5-1:",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congradulation! you guessed the right number.");
+}
+else {
+    console.log("Fail! you guess the wrong number");
+}
+;

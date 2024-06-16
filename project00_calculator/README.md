a648cf4ceeeca1a27e5f79b8eb4bfed08e1bce37
Muhammad waheed <162149796+muhammadwaheedaree@users.noreply.github.com>
2024-06-16 21:13:40 +0500

    Add files via upload

---
+++ project00_calculator/README.md
@@ -0,0 +1,14 @@
+## Simple Calculator using Inquirer in TypeScript
+
+This project is a simple calculator implemented in TypeScript, compiled to JavaScript. It utilizes the Inquirer library for building a command-line interface (CLI) to perform basic arithmetic operations.
+
+### Features:
+- Supports addition, subtraction, multiplication, and division.
+- Provides a user-friendly command-line interface using Inquirer.
+- Implemented in TypeScript for enhanced type safety and readability.
+
+### Usage:
+1. Clone the repository to your local machine.
+2. Install dependencies using npm install.
+3. Compile TypeScript to JavaScript using npm run build.
+4. Run the calculator using npm start.
---
+++ project00_calculator/index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const result = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        Message: "Select one of the operators to perform action operation",
+        type: "list",
+        name: "operator",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+if (result.operator === "+") {
+    console.log(result.firstNumber + result.secondNumber);
+}
+else if (result.operator === "-") {
+    console.log(result.firstNumber - result.secondNumber);
+}
+else if (result.operator === "*") {
+    console.log(result.firstNumber * result.secondNumber);
+}
+else if (result.operator === "/") {
+    console.log(result.firstNumber / result.secondNumber);
+}
+else {
+    console.log("Please select a valid operator");
+}
